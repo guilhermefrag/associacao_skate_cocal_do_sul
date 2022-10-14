25830e410c565c575a8940e0a79ec7f6473ed71d
guilhermefrag <guilhermefragnani@icloud.com>
2022-10-14 15:06:19 -0300

    subindo

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import { Tweet } from './components/Tweet'
+
+function App() {
+    const [tweets, setTweets] = useState<string[]>([
+        'Tweet 1',
+        'Tweet 2',
+        'Tweet 3',
+        'Tweet 4',
+    ])
+
+    function createTweet() {
+        setTweets([...tweets, 'New Tweet'])
+    };
+
+    return (
+        <div>
+        {tweets.map(tweet =>{
+            return <Tweet text={tweet} />
+        })}
+
+        <button onClick={createTweet}>Adicionar um Tweet</button>
+
+        </div>
+    )
+}
+
+export default App
+
