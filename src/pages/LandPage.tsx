530cc8e7b215988acc1613182311f2da4b120ee9
guilhermefrag <guilhermefragnani@icloud.com>
2022-11-01 11:54:21 -0300

    subindo

--- src/pages/LandPage.tsx
+++ src/pages/LandPage.tsx
@@ -3,15 +3,22 @@ import pista_skate_cocal from '../images/pista_skate_cocal.jpeg'
 
 export function LandPage() {
     return (
-        <div className="flex justify-center">
-            <div className='pb-20 pt-5'>
-                <img className='h-[50vw] w-[60vw]' src={pista_skate_cocal} alt="Pista de Skate Cocal do Sul"/>
-                <p className='h-[50vw] w-[60vw] text-justify text-[20px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt facere est itaque doloribus numquam a repellendus sed iste placeat ullam explicabo, voluptas tempore dolores quia, corrupti quos dignissimos, totam quo?
-                Minima officiis dignissimos ratione? Similique sequi nam, numquam, assumenda debitis porro quis maiores dolore fuga odio soluta ut beatae non illo deleniti cumque! Pariatur incidunt delectus, cum dolor minima explicabo!
-                A nulla, voluptatum magnam aliquam debitis doloribus hic libero, omnis culpa suscipit aperiam sed optio id reprehenderit ut, voluptatibus aliquid illo eos. Iste dolores vitae illo ex, nam et dolor.
-                Sit a tempore optio rem enim ipsa consequuntur voluptates laudantium ipsam, ad veritatis, mollitia expedita corporis quam. Voluptatum a similique dicta provident repellat, veritatis aliquid reprehenderit corrupti accusantium, voluptatem quam!
-                Fugit optio illo, aliquam quia consectetur rerum temporibus excepturi, amet expedita ab praesentium quaerat, nostrum in soluta ipsum ea libero exercitationem vero dolore doloremque a suscipit alias. Vitae, eligendi praesentium.</p>
-            </div>            
+        <div className=" flex-auto justify-center overflow-auto">
+            <div className='pb-20 pt-5 flex justify-center'>
+                <img className='h-[20vw] w-[25vw] pr-5' src={pista_skate_cocal} alt="Pista de Skate Cocal do Sul"/>
+                <p className='h-[20vw] w-[60vw] text-justify text-[20px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt facere est itaque doloribus numquam a repellendus sed iste placeat ullam explicabo, voluptas tempore dolores quia, corrupti quos dignissimos, totam quo?
+                Minima officiis dignissimos ratione? Similique sequi nam, numquam, assumenda debitis porro quis maiores dolore fuga odio soluta ut beatae non illo deleniti cumque! Pariatur incidunt delectus, cum dolor minima explicabo!</p>
+            </div>
+            <div className='pb-20 pt-5 flex justify-center'>
+                <p className='h-[25vw] w-[60vw] text-justify text-[20px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt facere est itaque doloribus numquam a repellendus sed iste placeat ullam explicabo, voluptas tempore dolores quia, corrupti quos dignissimos, totam quo?
+                Minima officiis dignissimos ratione? Similique sequi nam, numquam, assumenda debitis porro quis maiores dolore fuga odio soluta ut beatae non illo deleniti cumque! Pariatur incidunt delectus, cum dolor minima explicabo!</p>
+                <img className='h-[20vw] w-[25vw] pl-5' src={pista_skate_cocal} alt="Pista de Skate Cocal do Sul"/>
+            </div>
+            <div className='pb-20 pt-5 flex justify-center'>
+                <img className='h-[20vw] w-[25vw] pr-5' src={pista_skate_cocal} alt="Pista de Skate Cocal do Sul"/>
+                <p className='h-[20vw] w-[60vw] text-justify text-[20px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt facere est itaque doloribus numquam a repellendus sed iste placeat ullam explicabo, voluptas tempore dolores quia, corrupti quos dignissimos, totam quo?
+                Minima officiis dignissimos ratione? Similique sequi nam, numquam, assumenda debitis porro quis maiores dolore fuga odio soluta ut beatae non illo deleniti cumque! Pariatur incidunt delectus, cum dolor minima explicabo!</p>
+            </div>
         </div>
     )
 };
